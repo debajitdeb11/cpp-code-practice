60971d637466027c661e00d25ad3b55200c44b50
Debajit <debajitdeb11@gmail.com>
2020-02-08 16:58:57 +0530

    New file Added

---
+++ src14.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float a;
+    cin >> a;
+    cout << a << endl;
+    cout << (int)a <<endl; 
+    char b = static_cast<int>(a);
+    cout << b;
+    return 0; 
+
+}
