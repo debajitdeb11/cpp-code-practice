fc4a8b21fbabfc8095c25d73788845071f9f29a3
Debajit <debajitdeb11@gmail.com>
2020-04-03 06:53:42 +0530

    Insertion Updated!

--- src55.c
+++ src55.c
@@ -101,3 +101,11 @@ void displayList(struct Node* n){
         return 0;
     }
 
+/*
+
+
+Time complexity of append is O(n) where n is the number of nodes in linked list. 
+Since there is a loop from head to end, the function does O(n) work.
+This method can also be optimized to work in O(1) by keeping an extra pointer to tail of linked list
+
+*/
