63cbd23288ec7e09e79cfe85acb378b9b70e90ba
Debajit <debajitdeb11@gmail.com>
2020-04-09 12:16:37 +0530

    LinkedList: Loop Detection - Floyd's Loop Detection Method

---
+++ src73.cpp
@@ -0,0 +1,58 @@
+/*
+    LinkedList: Code to detect Loop Detection in LinkedList
+    Method : Floyd's Loop Detection
+    Time Complexitity : O(n)
+    Space Complexitity : Nil.
+    
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+void push(Node** head, int data){
+    Node* new_node = new Node(data);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int detectLoop(Node* head){
+    Node *tortoise = head, *hare = head;
+
+    while(tortoise && hare && hare->next){
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if(tortoise == hare){
+            cout << "Loop Found\n";
+            return 1;
+        }
+
+    }
+
+    return 0;
+}
+
+int main(){
+    Node* head = NULL;
+    push(&head, 10);
+    push(&head, 20);
+    push(&head, 30);
+    push(&head, 40);
+    push(&head, 50);
+
+    head->next->next->next->next->next = head;
+    detectLoop(head);
+
+    return 0;
+}
